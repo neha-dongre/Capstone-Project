d4d79bbe61c6eed8bc094ed3d86e81c4f8ea4817
NDD <128886954+neha-dongre@users.noreply.github.com>
2023-09-04 04:05:04 +0530

    added browse entertainment page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Genre from './Pages/Genre';
+import Movies from './Pages/Movies';
 import Browse from './Pages/Browse';
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="/Genre" element={<Genre />} />
+          <Route path="/movies" element={<Movies />} />
           <Route path='/browse' element={<Browse />} />
         </Routes>
       </BrowserRouter>
